f0dc733f7a799b1ea5b9c3f90693b497380091ee
Reginald Robinson <reggierobinson012@gmail.com>
2025-10-06 10:24:49 -0500

    se_project_todo-app

---
+++ components/FormValidation.js
@@ -0,0 +1,58 @@
+class FormValidatior {
+  constructor(settings, formEl) {
+    this._inputSelector = settings.inputSelector;
+    this._formSelector = settings.formSelector;
+    this._submitButtonSelector = settings.submitButtonSelector;
+    this._errorClass = settings.errorClass;
+    this._inputErrorClass = settings.inputErrorClass;
+    this._inactiveButtonClass = settings.inactiveButtonClass;
+    this._formEl = formEl;
+  }
+
+  // TODO - implement all the other methods
+
+  _checkInputValidity(inputElement) {
+    //  (1) TODO - implement this method
+    if (!inputElement.validity.valid) {
+      showInputError(
+        formElement,
+        inputElement,
+        inputElement.validationMessage,
+        settings
+      );
+    } else {
+      hideInputError(formElement, inputElement, settings);
+    }
+    // (b) work through errors in console as we did for enableValidation
+  }
+
+  setEventListeners() {
+    this._inputList = Array.from(
+      this._formEl.querySelectorAll(this._inputSelector)
+    );
+
+    // (2)TODO finish implementing _setEventListeners
+
+    // const buttonElement = formElement.querySelector(
+    //settings.submitButtonSelector
+    // );
+
+    //toggleButtonState(inputList, buttonElement, settings);
+
+    this._inputList.forEach((inputElement) => {
+      inputElement.addEventListener("input", () => {
+        this._checkInputValidity(inputElement);
+        // toggleButtonState(inputList, buttonElement, settings);
+      });
+    });
+  }
+
+  enableValidation() {
+    this._formEl.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+    });
+    this._setEventListeners(formElement, settings);
+  }
+}
+
+export default FormValidatior;
